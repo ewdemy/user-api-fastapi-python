27ad4413275df0874cb5b0048955181b97dbbf93
ewdemy <ewdemy@hotmail.com>
2022-01-04 22:16:46 -0300

    Model db

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 from fastapi import FastAPI
+from typing import List
+from models import User, Gender, Role
+
 
 app = FastAPI()
+
+db: List[User] = [
+    User(id = uuid4(), 
+        first_name = "Patricia", 
+        last_name = "Abravanel",
+        gender = Gender.female,
+        roles = [Role.student]
+    ),
+        User(id = uuid4(), 
+        first_name = "Jhon", 
+        last_name = "Welson",
+        middle_name = "Smith"
+        gender = Gender.male,
+        roles = [Role.user, Role.admin]
+    ),
+]
 
 @app.get("/")
 def root():
